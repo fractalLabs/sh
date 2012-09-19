1546ef6edc36f32a2a567f35552d0d3672352c37
nex <nex@pgr.lan>
2012-09-19 11:45:32 -0500

    cambie sh.core a sh

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :init-ns sh.core
+  :init-ns sh
   :dependencies [[org.clojure/clojure "1.4.0"]])
